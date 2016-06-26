0e975ea79364460138e35a0f957a2bd26210ee3d
Felix Franz <Felix.Franz@neobits.de>
2016-06-27 00:49:53 +0200

    Updated build.sh to really include all needed files & directories

--- build.sh
+++ build.sh
@@ -3,11 +3,18 @@
 mkdir -p tmp/rawcharts
 
 # Copy over all the needed things
-cp -r bin tmp/rawcharts
 cp -r appserver tmp/rawcharts
+cp -r bin tmp/rawcharts
 cp -r default tmp/rawcharts
+cp -r django tmp/rawcharts
 cp -r lookups tmp/rawcharts
+cp -r metadata tmp/rawcharts
+cp -r static tmp/rawcharts
 find tmp/rawcharts/ -name "*.pyc" -delete
+find tmp/rawcharts/ -name ".DS_Store" -delete
+find tmp/rawcharts/ -name ".git*" -delete
+rm -rf tmp/rawcharts/django/rawcharts/static/rawcharts/raw/.git
+
 
 cd tmp
 tar -czvf ../rawcharts.spl rawcharts
